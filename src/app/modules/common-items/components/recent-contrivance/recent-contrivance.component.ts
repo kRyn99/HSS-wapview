6da425ff0e16a41e56d88aa526f0c33d7c0d30ad
luuhung28 <hunglm0208@gmail.com>
2023-10-23 14:36:08 +0700

    fix home and detail

--- src/app/modules/common-items/components/recent-contrivance/recent-contrivance.component.ts
+++ src/app/modules/common-items/components/recent-contrivance/recent-contrivance.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input,  Renderer2,  ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, Input,  OnInit,  Renderer2,  ViewEncapsulation} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {ActivatedRoute, Router} from '@angular/router';
 import {TranslateService} from '@ngx-translate/core';
@@ -9,12 +9,12 @@ import { ContrivanceService } from '@app/shared/service/contrivance.service';
 
 SwiperCore.use([Keyboard, Pagination, Navigation, Virtual, Autoplay]);
 @Component({
-  selector: 'app-recent-contrivance',
+  selector: 'app-recent-contrivance-idea',
   templateUrl: './recent-contrivance.component.html',
   styleUrls: ['./recent-contrivance.component.scss'],
   encapsulation:ViewEncapsulation.None
 })
-export class RecentContrivanceComponent{
+export class RecentContrivanceIdeaComponent implements OnInit{
   constructor(
       private route:ActivatedRoute,
       private router: Router,
@@ -24,6 +24,15 @@ export class RecentContrivanceComponent{
       private renderer: Renderer2, private el: ElementRef
   ){
   }
+
+  ngOnInit(): void {
+    if(this.type =='idea'){
+      this.imgURL = 'assets/img/icon/good-idea2.png'
+    }else {
+      this.imgURL = 'assets/img/icon/good-idea.png'
+    }
+  }
+
   ngAfterViewInit() {
     // Lấy phần tử pagination
     const paginationElement = this.el.nativeElement.querySelector('.swiper-pagination');
@@ -44,18 +53,29 @@ export class RecentContrivanceComponent{
     hashNavigation: true,
     pagination: { clickable: true },
     centeredSlides: false,
-    initialSlide: 0
+    initialSlide: 0,
+    loop: true,
   };
   private subscriptions: Subscription[] = [];
   user: any;
   token: string;
-  @Input()listContrivance: any [] =[];
+  @Input()list: any [] =[];
+  @Input() type: string = '';
+  imgURL:String = '';
   viewDetail(id){
+    if(this.type == 'idea'){
+      this.contrivanceService.requestContrivanceIdSelected = id;
+      this.router.navigate(['idea/detail'],{
+        queryParams: { ideaId: id },
+      }); 
+      localStorage.setItem('ideaIdInLocalStorage', JSON.stringify(id));
+  }else if(this.type == 'contrivance'){
     this.contrivanceService.requestContrivanceIdSelected = id;
     this.router.navigate(["contrivance/detail"], {
       queryParams: { contrivanceId: id },
     });
     localStorage.setItem('contrivanceIdInLocalStorage', JSON.stringify(id));
   }
+}
 }
 
