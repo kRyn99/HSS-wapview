427feb2e8076095367163ba84134b211fad19b58
luuhung28 <hunglm0208@gmail.com>
2023-10-23 08:58:28 +0700

    code home page

--- src/app/modules/common-items/components/nav-menu/nav-menu.component.ts
+++ src/app/modules/common-items/components/nav-menu/nav-menu.component.ts
@@ -19,20 +19,20 @@ export class NavMenuComponent implements OnInit {
     private route: ActivatedRoute
   ) {}
   ngOnInit(): void {
-    // const currentUrl = this.router.routerState.snapshot.url;
-    // if (currentUrl.includes("idea")) {
-    //   this.homeService.isIdeaChecked.next(true);
-    //   this.homeService.isContrivanceChecked.next(false);
-    //   this.homeService.isHomeChecked.next(false);
-    // } else if (currentUrl.includes("contrivance")) {
-    //   this.homeService.isIdeaChecked.next(false);
-    //   this.homeService.isContrivanceChecked.next(true);
-    //   this.homeService.isHomeChecked.next(false);
-    // }else if(currentUrl.includes("home")){
-    //   this.homeService.isIdeaChecked.next(false);
-    //   this.homeService.isContrivanceChecked.next(false);
-    //   this.homeService.isHomeChecked.next(true);
-    // }
+    const currentUrl = this.router.routerState.snapshot.url;
+    if (currentUrl.includes("idea")) {
+      this.homeService.isIdeaChecked.next(true);
+      this.homeService.isContrivanceChecked.next(false);
+      this.homeService.isHomeChecked.next(false);
+    } else if (currentUrl.includes("contrivance")) {
+      this.homeService.isIdeaChecked.next(false);
+      this.homeService.isContrivanceChecked.next(true);
+      this.homeService.isHomeChecked.next(false);
+    }else if(currentUrl.includes("home")){
+      this.homeService.isIdeaChecked.next(false);
+      this.homeService.isContrivanceChecked.next(false);
+      this.homeService.isHomeChecked.next(true);
+    }
   }
 
   ideaRoute() {
